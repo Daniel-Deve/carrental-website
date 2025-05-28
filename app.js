bc5f12959f67cf5bd0dd2584756d413420b8f1d1
milez123p <milzswebprogram@gmail.com>
2025-05-28 04:51:37 +0100

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.querySelector('.menu-icon');
-    const navLinks = document.querySelector('.nav-links');
 
-    menuIcon.addEventListener('click', () => {
-      navLinks.classList.toggle('active');
-    });
-  });
+   function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
+  }
+
+
 
 //scroll button
     document.addEventListener("DOMContentLoaded", function () {
